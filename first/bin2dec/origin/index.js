a3e65377284eedfcfc9fcaa250ce6ece0c40f9f3
jczhou <1051160112@qq.com>
2020-04-21 20:48:22 +0800

    fix some logic problem and use es6

--- first/bin2dec/origin/index.js
+++ first/bin2dec/origin/index.js
@@ -1,40 +1,44 @@
-var getInput = document.getElementById('bnum');
-var setOutput=document.getElementsByTagName('output')[0];
-var state='';
-var originstate;
+const getInput = document.getElementById('bnum');
+const setOutput=document.getElementsByTagName('output')[0];
+
+// to save last input string
+let originstate='';
+//use es6
+
 function Bin2Dec(props){
     let sum=0;
     let tmp = 0;
+    // emtyp input
     if(!props){
         return;
     }
-    if (props[props.length - 1] != '0' && props[props.length - 1] != '1'){
-        getInput.value=originstate;
-        props=originstate;
+    // judge the lastest input number to be 0 or 1
+    else if (props[props.length - 1] != '0' && props[props.length - 1] != '1'){
+        props = props.slice(0, props.length-1);
+        getInput.value = props;
         alert('you should only input 0 or 1 !');
     }
+    // let input less than 8 character
     else if (props.length > 8) {
         alert('you should input less than 8 digits!');
         props= props.slice(0,8);
         getInput.value = getInput.value.slice(0,8);
     }
-
+    // calculate to equivalent dec number
     for(let char of props){
         tmp++;
-        
         if(char!='0'){
             sum+=Math.pow(2,props.length-tmp);
         }
     }
-    state = sum;
     originstate=props;
     return sum;
 }
-getInput.oninput = function (){
+
+getInput.oninput = ()=>{
     let toDec = Bin2Dec(getInput.value);
     setOutput.value = toDec;
-    if (!getInput.value){
-        setOutput.value='';
+    if (!getInput.value) {
+        setOutput.value = '';
     }
-    // console.log(toDec);
 }
